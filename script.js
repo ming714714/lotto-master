67dd0d58be57875e6a0cb193a5e13b0cf54a6133
ming714714 <ming714.tw@yahoo.com.tw>
2025-06-27 10:15:12 +0800

    Update to v7.1: enable real fetch + auto update via GitHub Actions

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-
-fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-    const latest = data[data.length - 1];
-    document.getElementById('latest').innerText = latest.numbers.join(', ');
-  })
-  .catch(() => {
-    document.getElementById('latest').innerText = '載入資料失敗';
-  });
+console.log('script loaded');
